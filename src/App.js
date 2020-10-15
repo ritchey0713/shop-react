0a2d50ae18e24fd12671b01841222f4ce02078d3
Dj <ritchey0713@gmail.com>
2020-10-15 11:44:12 -0400

    add auth calls

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from "react-router-dom"
 
 import './App.css';
@@ -6,18 +6,46 @@ import HomePage from './containers/homepage/homepage.component';
 import ShopPage from "./containers/shop/shop.component"
 import Header from "./components/header/header.component"
 import SignInAndSignUpPage from './containers/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from "./firebase/firebase.utils"
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={ HomePage } />
-        <Route exact path="/shop" component={ ShopPage } />
-        <Route exact path="/signin" component={ SignInAndSignUpPage } />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      currentUser: null
+    }
+  }
+  
+  unsubscribeFromAuth = null
+
+  componentDidMount(){
+    // auth comes from firebase 
+    // user in callback is user state
+    // auth is a subscription 
+    // set to unsubscribeFromAuth to call it as a function to close
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user })
+    })
+  }
+
+  componentWillUnmount(){
+   // only triggered to stop listening when leaving application.
+    this.unsubscribeFromAuth()
+  }
+
+
+  render(){
+    return (
+      <div>
+        <Header currentUser={ this.state.currentUser }/>
+        <Switch>
+          <Route exact path="/" component={ HomePage } />
+          <Route exact path="/shop" component={ ShopPage } />
+          <Route exact path="/signin" component={ SignInAndSignUpPage } />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
