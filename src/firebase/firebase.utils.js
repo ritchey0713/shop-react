257324fa1a3c3c106403784056f83f71c7c13c83
Dj <ritchey0713@gmail.com>
2020-10-16 11:33:33 -0400

    add create new user

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,6 +3,38 @@ import "firebase/firestore"
 import "firebase/auth"
 import config from "./firebase.secrets"
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  //if no auth obj comes through terminate func
+  if(!userAuth) return;
+
+  //queryRef
+  const userRef = firestore.doc(`users/${userAuth.uid}`) 
+  // console.log(firestore.doc('users/1234vefve'))
+  //userRef (document reference) - (used for performing CRUD actions)
+  const snapShot = await userRef.get()
+  // console.log(snapShot)
+
+  // creates new user if it isnt already in db
+  if(!snapShot.exists){
+    const { displayName, email } = userAuth
+    const createdAt = new Date()
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch(e) {
+      console.log("ERROR CREATEING USER", e.message)
+    }
+  }
+
+  return userRef
+
+}
+
 firebase.initializeApp(config)
 
 //google auth 
