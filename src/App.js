51e9abfe7eab37d90a1bfa1dd6222c39d28f5742
Dj <ritchey0713@gmail.com>
2020-10-16 11:33:43 -0400

    build out async auth

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from './containers/homepage/homepage.component';
 import ShopPage from "./containers/shop/shop.component"
 import Header from "./components/header/header.component"
 import SignInAndSignUpPage from './containers/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from "./firebase/firebase.utils"
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils"
 
 class App extends Component {
   constructor(){
@@ -23,8 +23,30 @@ class App extends Component {
     // user in callback is user state
     // auth is a subscription 
     // set to unsubscribeFromAuth to call it as a function to close
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      // this.setState({ currentUser: user })
+      // createUserProfileDocument(userAuth)
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
+        //get a snapshop using .onSnapShot
+        userRef.onSnapshot(snapShot => {
+          // .data() gives us json of everything BUT the obj id
+          // console.log(snapShot.data())
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        })
+      } else {
+        //if we have no userAuth reset currentuser to null, 
+        // this is also triggered by the auth.signOut() in the header
+        this.setState({
+          currentUser: null
+        })
+
+      }
     })
   }
 
