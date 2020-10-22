c0d5f8857de73392aef58828d272c68dbe1b5143
Dj <ritchey0713@gmail.com>
2020-10-22 11:14:58 -0400

    add inverted styles conditional

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,11 +1,20 @@
-import React from "react"
-import "./custom-button.styles.scss"
+import React from "react";
+import "./custom-button.styles.scss";
 
 //children prop allows you to grab w/e is in between opening and closing tags
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
-  <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button `} {...otherProps}>
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => (
+  <button
+    className={`${inverted ? "inverted" : ""}
+    ${isGoogleSignIn ? "google-sign-in" : ""} custom-button `}
+    {...otherProps}
+  >
     {children}
   </button>
-)
+);
 
 export default CustomButton;
