8ad16144e50ea279770ee9baedb5b60ae6209abd
Dj <ritchey0713@gmail.com>
2020-10-22 11:24:07 -0400

    restructure to pass item obj as a prop

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,21 +1,18 @@
-import React from "react"
-import "./collection-preview.styles.scss"
-import CollectionItem from "../collection-item/collection-item.component"
+import React from "react";
+import "./collection-preview.styles.scss";
+import CollectionItem from "../collection-item/collection-item.component";
 
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
-    <h1 className="title">{ title.toUpperCase() }</h1>
+    <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
-      {
-        items
-          .filter((item, index) => index < 4)
-          .map(({ id, ...otherItemProps}) => (
-            <CollectionItem key={ id } { ...otherItemProps }/>
-            
-          ))
-      }
+      {items
+        .filter((item, index) => index < 4)
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
     </div>
   </div>
-)
+);
 
-export default CollectionPreview
+export default CollectionPreview;
