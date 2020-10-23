fd48f306c57d6696180775aa9d4bb4fe6ecd5e0b
Dj <ritchey0713@gmail.com>
2020-10-23 11:42:32 -0400

    create basic selectors

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,22 @@
+import { createSelector } from "reselect";
+
+//input selector (returns a slice of the state)
+const selectCart = (state) => state.cart;
+
+// takes 2 args, a collection, and a callback to return data
+// callback args are from the collection( whatever pieces of state we pass to it)
+
+export const selectCartitems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  // can use any collection, such as just the collection of cartItems since we dont need the hidden field
+  [selectCartitems],
+  (cartItems) =>
+    cartItems.reduce(
+      (accQuantity, cartItem) => accQuantity + cartItem.quantity,
+      0
+    )
+);
