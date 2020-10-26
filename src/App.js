a4588389d45350c0b99e2f941a7b696c4d109548
Dj <ritchey0713@gmail.com>
2020-10-26 13:16:02 -0400

    refactor to use selector

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./containers/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
+import { createStructuredSelector } from "reselect";
+import { selectCurrentUser } from "./redux/user/user.selectors";
 
 class App extends Component {
   unsubscribeFromAuth = null;
@@ -73,8 +75,8 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser,
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
 });
 
 export default connect(mapStateToProps, { setCurrentUser })(App);
