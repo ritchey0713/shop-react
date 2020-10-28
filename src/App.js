4b8334561763a36702ed615cce702eee43c8bd3b
Dj <ritchey0713@gmail.com>
2020-10-28 14:47:29 -0400

    shop has nested route, do not use exact

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ class App extends Component {
         <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route exact path="/shop" component={ShopPage} />
+          <Route path="/shop" component={ShopPage} />
           <Route exact path="/checkout" component={CheckoutPage} />
 
           <Route
