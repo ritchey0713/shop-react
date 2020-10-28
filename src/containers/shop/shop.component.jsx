7045093a224b4e56982e8560ea1d8d0c12e1512d
Dj <ritchey0713@gmail.com>
2020-10-28 14:47:40 -0400

    add collection dynamic url

--- src/containers/shop/shop.component.jsx
+++ src/containers/shop/shop.component.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import CollectionPage from "../../containers/collection/collection.component";
 
-const ShopPage = ({ collections }) => (
-  <div className="shop=-page">
-    <CollectionsOverview />
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
