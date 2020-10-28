2a5fde593160663d415b65f9984ed17670b59ed0
Dj <ritchey0713@gmail.com>
2020-10-28 12:11:41 -0400

    change rootreducer to use persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,20 @@
 import { combineReducers } from "redux";
 import cartReducer from "./cart/cart.reducer";
 import userReducer from "./user/user.reducer";
+import { persistReducer } from "redux-persist";
+//gives us access to local storage
+import storage from "redux-persist/lib/storage";
 
-export default combineReducers({
+//need a config to tell it which reducers we want to persist
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
