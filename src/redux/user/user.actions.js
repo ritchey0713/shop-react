23862321103b72d73bbf425471da4b0e18f5fa67
Dj <ritchey0713@gmail.com>
2020-10-29 12:02:04 -0400

    clean up

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,7 +1,6 @@
 import { userActionTypes } from "./user.types";
 
 export const setCurrentUser = (user) => {
-  console.log("action hit");
   return {
     type: userActionTypes.SET_CURRENT_USER,
     payload: user,
