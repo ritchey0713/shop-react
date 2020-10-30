eafdf24c8e1d2464c114592416f3ffeb41ee4f23
Dj <ritchey0713@gmail.com>
2020-10-30 14:44:38 -0400

    add icon styles

---
+++ src/components/cart-icon/cart-icon.styles.jsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+// cart icon div 
+// item count span
+
+export const StyledCartIcon = styled.div`
+  width: 45px;
+  height: 45px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  .shopping-icon {
+    width: 24px;
+    height: 24px;
+  }
+`
+
+export const StyledItemCount = styled.span`
+ position: absolute;
+  font-size: 10px;
+  font-weight: bold;
+  bottom: 12px;
+`
+
+export const StyledIconSvg = styled.svg`
+  width: 24px;
+  height: 24px;
+`
