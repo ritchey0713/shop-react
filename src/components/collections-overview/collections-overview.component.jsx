af22c4b2a2afc83629f4cc1af391a891303068e9
Dj <ritchey0713@gmail.com>
2020-11-02 13:20:19 -0500

    render new styles

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -4,14 +4,15 @@ import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
 import { createStructuredSelector } from "reselect";
 import CollectionPreview from "../collection-preview/collection-preview.component";
 import "./collections-overview.styles.scss";
+import { StyledOverview } from "./collections-overview.styles"
 
 const CollectionOverview = ({ collections }) => {
   return (
-    <div className="collections-overview">
+    <StyledOverview>
       {collections.map(({ id, ...otherCollectionProps }) => (
         <CollectionPreview key={id} {...otherCollectionProps} />
       ))}
-    </div>
+    </StyledOverview>
   );
 };
 
