d67bf0c470bb853684dc6054f60f450a5e40e8c6
Dj <ritchey0713@gmail.com>
2020-11-02 13:34:07 -0500

    render new styles and create classes

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,20 +1,21 @@
 import React from "react"
 import "./form-input.styles.scss"
+import { StyledFormInput, StyledGroup, StyledLabel } from "./form-input.styles"
 
 //otherProps.value.length checks if user has started typing into form field
 
 const FormInput = ({ handleChange, label, ...otherProps }) => (
-  <div className="group">
-    <input className="form-input" onChange={ handleChange } { ...otherProps }/>
+  <StyledGroup>
+    <StyledFormInput onChange={ handleChange } { ...otherProps }/>
     {
       label ? 
-      <label className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}>
+      <StyledLabel className={otherProps.value.length ? "shrink" : ""}>
         { label }
-      </label>
+      </StyledLabel>
       : 
       null
     }
-  </div>
+  </StyledGroup>
 )
 
 export default FormInput
