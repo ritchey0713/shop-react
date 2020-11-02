9340f1483f65d23758ce62ff591b4884fa03af88
Dj <ritchey0713@gmail.com>
2020-11-02 15:55:44 -0500

    add styles remove unused

---
+++ src/containers/collection/collection.styles.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+// collection page div 
+//h2 title 
+//items div
+
+export const StyledCollection = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const StyledTitle = styled.h2`
+  font-size: 38px;
+  margin: 0 auto 30px;
+`
+
+export const StyledItems = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-gap: 10px;
+    margin-bottom: 30px;
+`
