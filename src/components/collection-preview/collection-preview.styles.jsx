0c5bd577f9f528a61f356f507e6218ace247a639
Dj <ritchey0713@gmail.com>
2020-11-02 13:17:02 -0500

    create preview styles

---
+++ src/components/collection-preview/collection-preview.styles.jsx
@@ -0,0 +1,22 @@
+//div collection-preview
+// h1 title
+// div preview 
+//
+
+import styled from 'styled-components';
+
+export const StyledCollectionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 30px;
+`
+
+export const StyledTitle = styled.h1`
+  font-size: 28px;
+  margin-bottom: 25px;
+`
+
+export const StyledPreview = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
