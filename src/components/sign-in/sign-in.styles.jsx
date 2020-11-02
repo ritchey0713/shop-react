94234256e69bb578b68a16b4128a7bb70a604d4f
Dj <ritchey0713@gmail.com>
2020-11-02 14:05:15 -0500

    add new styles

---
+++ src/components/sign-in/sign-in.styles.jsx
@@ -0,0 +1,16 @@
+// div sign in 
+// 
+// buttons div
+
+import styled from 'styled-components';
+
+export const StyledSignUp = styled.div`
+  width: 380px;
+  display: flex;
+  flex-direction: column;
+`
+
+export const StyledButtonsWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
