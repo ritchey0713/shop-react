f2e34643d26f71e36aa81504b759eb491cb80501
Dj <ritchey0713@gmail.com>
2020-11-02 13:09:58 -0500

    create styles

---
+++ src/components/collection-item/collection-item.styles.jsx
@@ -0,0 +1,62 @@
+// coll item div
+//img div
+//footer div
+//span name
+//span price
+
+
+import styled from 'styled-components';
+import CustomButton from '../custom-button/custom-button.component';
+
+export const StyledCollectionItem = styled.div`
+  width: 22vw;
+  display: flex;
+  flex-direction: column;
+  height: 350px;
+  align-items: center;
+  position: relative;
+  margin-top: 25px;
+  &:hover {
+    .image {
+      opacity: .75;
+    }
+    button {
+      opacity: .75;
+      display: flex;
+
+    }
+  }
+`
+
+export const StyledImage = styled.div`
+  width: 100%;
+  height: 95%;
+  background-size: cover;
+  background-position: center;
+  margin-bottom: 10px;
+  background-image: ${({ imageUrl }) => `url(${imageUrl})`}
+`
+
+export const AddButton = styled(CustomButton)`
+  width: 80%;
+  position: absolute;
+  top: 255px;
+  display: none;
+`
+export const StyledFooter = styled.div`
+  width: 100%;
+  height: 5%;
+  display: flex;
+  justify-content: space-between;
+  font-size: 18px;
+`
+
+export const StyledName = styled.span`
+  width: 90%;
+  margin-bottom: 15px;
+`
+
+export const StyledPrice = styled.span`
+  width: 10%;
+  text-align: right;
+`
