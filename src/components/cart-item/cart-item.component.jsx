e86985c04f57840360e48a81dc35b7eb146a5e7e
Dj <ritchey0713@gmail.com>
2020-11-02 12:19:54 -0500

    use styled components

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import { StyledImg, StyledDetails, StyledItem } from "./cart-item.styles"
 
 import "./cart-item.styles.scss";
 
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
-  <div className="cart-item">
-    <img src={imageUrl} alt="item" />
-    <div className="item-details">
-      <span className="name">{name}</span>
-      <span className="price">
+  <StyledItem>
+    <StyledImg src={imageUrl} alt="item" />
+    <StyledDetails>
+      <span>{name}</span>
+      <span>
         {quantity} X ${price}
       </span>
-    </div>
-  </div>
+    </StyledDetails>
+  </StyledItem>
 );
 
 export default CartItem;
