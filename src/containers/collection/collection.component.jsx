22db8086937c69675d92c04a646aedd7e2f8c16c
Dj <ritchey0713@gmail.com>
2020-11-02 15:55:49 -0500

    render styles

--- src/containers/collection/collection.component.jsx
+++ src/containers/collection/collection.component.jsx
@@ -3,18 +3,19 @@ import { connect } from "react-redux";
 import { selectCollection } from "../../redux/shop/shop.selectors";
 import CollectionItem from "../../components/collection-item/collection-item.component";
 import "./collection.styles.scss";
+import { StyledCollection, StyledItems, StyledTitle } from "./collection.styles";
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
   return (
-    <div className="collection-page">
-      <h2 className="title">{title}</h2>
-      <div className="items">
+    <StyledCollection>
+      <StyledTitle>{title}</StyledTitle>
+      <StyledItems>
         {items.map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
-      </div>
-    </div>
+      </StyledItems>
+    </StyledCollection>
   );
 };
 
