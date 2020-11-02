d1513f25a967599962ba5c375c7a455dadaa1384
Dj <ritchey0713@gmail.com>
2020-11-02 13:17:10 -0500

    render using new components

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import "./collection-preview.styles.scss";
 import CollectionItem from "../collection-item/collection-item.component";
+import { StyledCollectionWrapper, StyledPreview, StyledTitle } from "./collection-preview.styles"
 
 const CollectionPreview = ({ title, items }) => (
-  <div className="collection-preview">
-    <h1 className="title">{title.toUpperCase()}</h1>
-    <div className="preview">
+  <StyledCollectionWrapper>
+    <StyledTitle>{title.toUpperCase()}</StyledTitle>
+    <StyledPreview>
       {items
         .filter((item, index) => index < 4)
         .map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
-    </div>
-  </div>
+    </StyledPreview>
+  </StyledCollectionWrapper>
 );
 
 export default CollectionPreview;
