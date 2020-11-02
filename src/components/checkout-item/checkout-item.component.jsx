a5f63287ae6cfc2e0d671107c86e4e3bbfa2530a
Dj <ritchey0713@gmail.com>
2020-11-02 13:11:26 -0500

    render styles

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -6,29 +6,30 @@ import {
   addItem,
   removeItem,
 } from "../../redux/cart/cart.actions.js";
+import { StyledArrow, StyledButton, StyledImg, StyledImgWrapper, StyledItemDiv, StyledQuantitySpan, StyledSpan, StyledValue } from "./checkout-item.styles"
 
 const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
   const { imageUrl, price, name, quantity } = cartItem;
   return (
-    <div className="checkout-item">
-      <div className="image-container">
-        <img src={imageUrl} alt="item" />
-      </div>
-      <span className="name">{name}</span>
-      <span className="quantity">
-        <div className="arrow" onClick={() => removeItem(cartItem)}>
+    <StyledItemDiv>
+      <StyledImgWrapper>
+        <StyledImg src={imageUrl} alt="item" />
+      </StyledImgWrapper>
+      <StyledSpan>{name}</StyledSpan>
+      <StyledQuantitySpan>
+        <StyledArrow onClick={() => removeItem(cartItem)}>
           &#10094;
-        </div>
-        <span className="value">{quantity}</span>
-        <div className="arrow" onClick={() => addItem(cartItem)}>
+        </StyledArrow>
+        <StyledValue>{quantity}</StyledValue>
+        <StyledArrow onClick={() => addItem(cartItem)}>
           &#10095;
-        </div>
-      </span>
-      <span className="price"> {price}</span>
-      <div className="remove-button" onClick={() => clearItem(cartItem)}>
+        </StyledArrow>
+      </StyledQuantitySpan>
+      <StyledSpan> {price}</StyledSpan>
+      <StyledButton onClick={() => clearItem(cartItem)}>
         &#10007;
-      </div>
-    </div>
+      </StyledButton>
+    </StyledItemDiv>
   );
 };
 
