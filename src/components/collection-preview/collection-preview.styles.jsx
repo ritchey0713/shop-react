b6da787136102c72384e9221c18d57c71dbbc1ca
Dj <ritchey0713@gmail.com>
2020-11-03 13:11:13 -0500

    change comp to be a Link

--- src/components/collection-preview/collection-preview.styles.jsx
+++ src/components/collection-preview/collection-preview.styles.jsx
@@ -1,22 +1,23 @@
 //div collection-preview
 // h1 title
-// div preview 
+// div preview
 //
 
-import styled from 'styled-components';
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const StyledCollectionWrapper = styled.div`
   display: flex;
   flex-direction: column;
   margin-bottom: 30px;
-`
+`;
 
-export const StyledTitle = styled.h1`
+export const StyledTitle = styled(Link)`
   font-size: 28px;
   margin-bottom: 25px;
-`
+`;
 
 export const StyledPreview = styled.div`
   display: flex;
   justify-content: space-between;
-`
+`;
