e520693af3356bf1bd36486f559bf82507537d4a
Dj <ritchey0713@gmail.com>
2020-11-03 13:10:31 -0500

    add HOC loading to await fetch from firebase

--- src/containers/shop/shop.component.jsx
+++ src/containers/shop/shop.component.jsx
@@ -8,8 +8,16 @@ import {
   convertCollectionsSnapShotToMap,
 } from "../../firebase/firebase.utils";
 import { startUpdateCollections } from "../../redux/shop/shop.actions";
+import WithSpinner from "../../components/with-spinner/with-spinner.component";
+
+// pass collectionsoverview to with spinner to be the component rendered if loading is false
+const CollectionOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionsPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends Component {
+  state = {
+    loading: true,
+  };
   unsubscribeFromSnapShop = null;
 
   componentDidMount() {
@@ -20,18 +28,33 @@ class ShopPage extends Component {
       async (snapShot) => {
         const collectionsMap = convertCollectionsSnapShotToMap(snapShot);
         startUpdateCollections(collectionsMap);
+        this.setState({
+          loading: false,
+        });
       }
     );
   }
 
   render() {
     const { match } = this.props;
+    const { loading } = this.state;
     return (
       <div>
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route
+          exact
+          path={`${match.path}`}
+          render={(props) => (
+            <CollectionOverviewWithSpinner
+              isLoading={loading}
+              path={props.match.path}
+            />
+          )}
+        />
         <Route
           path={`${match.path}/:collectionId`}
-          component={CollectionPage}
+          render={(props) => (
+            <CollectionsPageWithSpinner isLoading={loading} {...props} />
+          )}
         />
       </div>
     );
