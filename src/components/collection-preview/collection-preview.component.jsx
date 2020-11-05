c18c212a7621330224a9eb409f62328ccc17aca9
Dj <ritchey0713@gmail.com>
2020-11-05 14:30:03 -0500

    refactor

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -7,19 +7,21 @@ import {
   StyledTitle,
 } from "./collection-preview.styles";
 
-const CollectionPreview = ({ title, items, ...otherProps }) => (
-  <StyledCollectionWrapper>
-    <StyledTitle to={`${otherProps.path}/${otherProps.routeName}`}>
-      {title.toUpperCase()}
-    </StyledTitle>
-    <StyledPreview>
-      {items
-        .filter((item, index) => index < 4)
-        .map((item) => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
-    </StyledPreview>
-  </StyledCollectionWrapper>
-);
+const CollectionPreview = ({ title, items, ...otherProps }) => {
+  return (
+    <StyledCollectionWrapper>
+      <StyledTitle to={`${otherProps.path}/${otherProps.routeName}`}>
+        {title.toUpperCase()}
+      </StyledTitle>
+      <StyledPreview>
+        {items
+          .filter((item, index) => index < 4)
+          .map((item) => (
+            <CollectionItem key={item.id} item={item} />
+          ))}
+      </StyledPreview>
+    </StyledCollectionWrapper>
+  );
+};
 
 export default CollectionPreview;
