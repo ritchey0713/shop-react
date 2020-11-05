8baef8d12015e4bb7e6d478bc8f3d68c2e4c999e
Dj <ritchey0713@gmail.com>
2020-11-05 10:41:25 -0500

    add thunk to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { persistStore } from "redux-persist";
-
+import thunk from "redux-thunk";
 import rootReducer from "./root-reducer";
 
 // set as array to add addtional middlewares later
 //logger added for debugging purposes
-const middlewares = [logger];
+const middlewares = [logger, thunk];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
