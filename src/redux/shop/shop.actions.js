5c02b2afe8dfff791069fc7b8b903edcf12a8ac6
Dj <ritchey0713@gmail.com>
2020-11-05 12:00:22 -0500

    add new thunk actions

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,34 @@
 import ShopActionTypes from "./shop.types";
+import {
+  firestore,
+  convertCollectionsSnapShotToMap,
+} from "../../firebase/firebase.utils";
 
-export const startUpdateCollections = (collectionsMap) => ({
-  type: ShopActionTypes.UPDATE_COLLECTIONS,
+export const fetchCollectionsStart = () => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START,
+});
+
+export const fetchCollectionsSuccess = (collectionsMap) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
   payload: collectionsMap,
 });
+
+export const fetchCollectionFailure = (error) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: error,
+});
+
+export const fetchCollectionsStartAync = () => {
+  return (dispatch) => {
+    const collectionRef = firestore.collection("collections");
+    dispatch(fetchCollectionsStart());
+
+    collectionRef
+      .get()
+      .then((snapshot) => {
+        const collectionsMap = convertCollectionsSnapShotToMap(snapshot);
+        dispatch(fetchCollectionsSuccess(collectionsMap));
+      })
+      .catch((error) => dispatch(fetchCollectionFailure(error.message)));
+  };
+};
