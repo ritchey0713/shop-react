6effd2a043dc6dd0638c22f94949a2e988144348
Dj <ritchey0713@gmail.com>
2020-11-06 12:51:32 -0500

    add redux sagas to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,28 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { persistStore } from "redux-persist";
-import thunk from "redux-thunk";
+//import thunk from "redux-thunk";
 import rootReducer from "./root-reducer";
+import createSagaMiddleware from "redux-saga";
+import { fetchCollectionsStart } from "./shop/shop.saga";
 
 // set as array to add addtional middlewares later
 //logger added for debugging purposes
-const middlewares = [logger, thunk];
+// replace thunk with sagas
+//const middlewares = [thunk];
+
+// create sagas
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(fetchCollectionsStart);
 
 // allows us to save redux store to local/session storage
 export const persistor = persistStore(store);
