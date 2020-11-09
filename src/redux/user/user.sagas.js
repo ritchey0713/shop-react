5b72f6b0f54292a095ec1ba4f13808dfb9351329
Dj <ritchey0713@gmail.com>
2020-11-09 12:16:39 -0500

    add in async google sign in sagas

---
+++ src/redux/user/user.sagas.js
@@ -0,0 +1,30 @@
+import { takeLatest, put, all, call } from "redux-saga/effects";
+import userActionTypes from "./user.types";
+import { googleSignInSuccess, googleSignInFailure } from "./user.actions";
+
+import {
+  googleProvider,
+  auth,
+  createUserProfileDocument,
+} from "../../firebase/firebase.utils";
+
+export function* signInWithGoogle() {
+  try {
+    const { user } = yield auth.signInWithPopup(googleProvider);
+    const userRef = yield call(createUserProfileDocument, user);
+    const userSnapShot = yield userRef.get();
+    yield put(
+      googleSignInSuccess({ id: userSnapShot.id, ...userSnapShot.data() })
+    );
+  } catch (error) {
+    yield put(googleSignInFailure(error));
+  }
+}
+
+export function* onGoogleSignInStart() {
+  yield takeLatest(userActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
+}
+
+export function* userSagas() {
+  yield all([call(onGoogleSignInStart)]);
+}
