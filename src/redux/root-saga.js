e08381091e7937e09737d1558790117534b31f3d
Dj <ritchey0713@gmail.com>
2020-11-09 12:17:09 -0500

    call user Sagas

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -1,8 +1,9 @@
 //run all sagas at app start up
 import { all, call } from "redux-saga/effects";
 import { fetchCollectionStart } from "./shop/shop.saga";
+import { userSagas } from "./user/user.sagas";
 
 export default function* rootSaga() {
   // all takes array of sagas to listen for, each ele should call the saga to initialize
-  yield all([call(fetchCollectionStart)]);
+  yield all([call(fetchCollectionStart), call(userSagas)]);
 }
