2d291335ed86d72e12a8b9393732eacc4cb1f8bb
Dj <ritchey0713@gmail.com>
2020-11-09 12:34:47 -0500

    refactor google sign in to use sagas

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,74 +1,79 @@
-import React, { Component } from "react"
-import { auth, signInWithGoogle } from "../../firebase/firebase.utils"
-import CustomButton from "../custom-button/custom-button.component"
-import FormInput from "../form-input/form-input.component"
-import { StyledButtonsWrapper, StyledSignUp } from "./sign-in.styles"
-import "./sign-in.styles.scss"
-
+import React, { Component } from "react";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
+import CustomButton from "../custom-button/custom-button.component";
+import FormInput from "../form-input/form-input.component";
+import { StyledButtonsWrapper, StyledSignUp } from "./sign-in.styles";
+import "./sign-in.styles.scss";
+import { googleSignInStart } from "../../redux/user/user.actions.js";
+import { connect } from "react-redux";
 
 class SignIn extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       email: "",
-      password: ""
-    }
+      password: "",
+    };
   }
 
-  handleSignInSubmit = async e => {
-    e.preventDefault()
+  handleSignInSubmit = async (e) => {
+    e.preventDefault();
 
-    const { email, password } = this.state
+    const { email, password } = this.state;
 
     try {
-      await auth.signInWithEmailAndPassword(email, password)
-      this.setState({ email: "", password: "" })
-
-    } catch(e) {
-      console.log("ERROR SIGNING IN", e.message)
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (e) {
+      console.log("ERROR SIGNING IN", e.message);
     }
+  };
 
-  }
+  handleSignInChange = (e) => {
+    const { value, name } = e.target;
+    this.setState({ [name]: value });
+  };
 
-  handleSignInChange = e => {
-    const { value, name } = e.target
-    this.setState({ [name]: value })
-  }
-
-  render(){
-    return(
+  render() {
+    const { googleSignInStart } = this.props;
+    return (
       <StyledSignUp>
-        <h2>
-          I already have an account
-        </h2>
+        <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
         <form onSubmit={this.handleSignInSubmit}>
-          <FormInput name="email" 
-            type="email" 
-            value={ this.state.email } 
-            required 
-            handleChange={this.handleSignInChange }
+          <FormInput
+            name="email"
+            type="email"
+            value={this.state.email}
+            required
+            handleChange={this.handleSignInChange}
             label="email"
           />
-          
-          <FormInput name="password" 
-            type="password" 
-            value={ this.state.password } 
-            required 
-            handleChange={this.handleSignInChange }
+
+          <FormInput
+            name="password"
+            type="password"
+            value={this.state.password}
+            required
+            handleChange={this.handleSignInChange}
             label="password"
           />
-          
+
           <StyledButtonsWrapper>
             <CustomButton type="submit">Sign In</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign In with google</CustomButton>
+            <CustomButton
+              type="button"
+              onClick={googleSignInStart}
+              isGoogleSignIn
+            >
+              Sign In with google
+            </CustomButton>
           </StyledButtonsWrapper>
-
         </form>
       </StyledSignUp>
-    )
+    );
   }
 }
 
-export default SignIn
+export default connect(null, { googleSignInStart })(SignIn);
