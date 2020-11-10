a93f0a3bf930776f967cc6558bdc5fdc34051e2e
Dj <ritchey0713@gmail.com>
2020-11-10 16:26:06 -0500

    add new actions for sign up

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -12,6 +12,11 @@ export const googleSignInStart = () => ({
   type: userActionTypes.GOOGLE_SIGN_IN_START,
 });
 
+export const emailSignInStart = (emailAndPassword) => ({
+  type: userActionTypes.EMAIL_SIGN_IN_START,
+  payload: emailAndPassword,
+});
+
 export const signInSuccess = (user) => ({
   type: userActionTypes.SIGN_IN_SUCCESS,
   payload: user,
@@ -21,9 +26,20 @@ export const signInFailure = (error) => ({
   type: userActionTypes.SIGN_IN_FAILURE,
   payload: error,
 });
-export const emailSignInStart = (emailAndPassword) => ({
-  type: userActionTypes.EMAIL_SIGN_IN_START,
-  payload: emailAndPassword,
+
+export const emailSignUpStart = (userInfo) => ({
+  type: userActionTypes.SIGN_UP_START,
+  payload: userInfo,
+});
+
+export const signUpSuccess = ({ user, additionalData }) => ({
+  type: userActionTypes.SIGN_UP_SUCCESS,
+  payload: { user, additionalData },
+});
+
+export const signUpFailure = (error) => ({
+  type: userActionTypes.SIGN_UP_FAILURE,
+  payload: error,
 });
 
 export const checkUserSession = () => ({
