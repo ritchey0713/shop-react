dd9f7b6007a2ea966ece6b28dbdf467e354a9c42
Dj <ritchey0713@gmail.com>
2020-11-10 16:26:28 -0500

    add shopsagas to root saga

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -1,9 +1,10 @@
 //run all sagas at app start up
 import { all, call } from "redux-saga/effects";
-import { fetchCollectionStart } from "./shop/shop.saga";
+import { cartSagas } from "./cart/cart.sagas";
+import { shopSagas } from "./shop/shop.saga";
 import { userSagas } from "./user/user.sagas";
 
 export default function* rootSaga() {
   // all takes array of sagas to listen for, each ele should call the saga to initialize
-  yield all([call(fetchCollectionStart), call(userSagas)]);
+  yield all([call(shopSagas), call(userSagas), call(cartSagas)]);
 }
