d6185505d7cf55ebf49d4182be05952547b05b0b
Dj <ritchey0713@gmail.com>
2020-11-11 14:53:28 -0500

    refactor use use hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
@@ -7,83 +8,81 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import CheckoutPage from "./pages/check-out/check-out.component";
+//import CheckoutPage from "./pages/check-out/check-out.component";
 import { createStructuredSelector } from "reselect";
 import { selectCurrentUser } from "./redux/user/user.selectors";
 import { selectCollectionsForPreview } from "./redux/shop/shop.selectors";
 import CheckOutContainer from "./pages/check-out/check-out.container";
 import { checkUserSession } from "./redux/user/user.actions";
-class App extends Component {
-  unsubscribeFromAuth = null;
-
-  componentDidMount() {
-    const { setCurrentUser, collections, checkUserSession } = this.props;
+const App = ({ checkUserSession, currentUser }) => {
+  // pass [checkUserSession as it is passed in from mapDIspatch, so it wont allow it to fire several times]
+  useEffect(() => {
     checkUserSession();
+  }, [checkUserSession]);
+  // pre hooks
+  // unsubscribeFromAuth = null;
+  // componentDidMount() {
+  //   const { setCurrentUser, collections, checkUserSession } = this.props;
+  //   checkUserSession();
 
-    // auth comes from firebase
-    // user in callback is user state
-    // auth is a subscription
-    // set to unsubscribeFromAuth to call it as a function to close
-    // USING SUBCRIBING TO GET CURRENT USER (PRE SAGA)
-    // this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
-    //   // this.setState({ currentUser: user })
-    //   // createUserProfileDocument(userAuth)
-    //   if (userAuth) {
-    //     const userRef = await createUserProfileDocument(userAuth);
-    //     //get a snapshop using .onSnapShot
-    //     userRef.onSnapshot((snapShot) => {
-    //       // .data() gives us json of everything BUT the obj id
-    //       // console.log(snapShot.data())
-    //       setCurrentUser({
-    //         id: snapShot.id,
-    //         ...snapShot.data(),
-    //       });
-    //     });
-    //   } else {
-    //     //if we have no userAuth reset currentuser to null,
-    //     // this is also triggered by the auth.signOut() in the header
+  //   // auth comes from firebase
+  //   // user in callback is user state
+  //   // auth is a subscription
+  //   // set to unsubscribeFromAuth to call it as a function to close
+  //   // USING SUBCRIBING TO GET CURRENT USER (PRE SAGA)
+  //   // this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+  //   //   // this.setState({ currentUser: user })
+  //   //   // createUserProfileDocument(userAuth)
+  //   //   if (userAuth) {
+  //   //     const userRef = await createUserProfileDocument(userAuth);
+  //   //     //get a snapshop using .onSnapShot
+  //   //     userRef.onSnapshot((snapShot) => {
+  //   //       // .data() gives us json of everything BUT the obj id
+  //   //       // console.log(snapShot.data())
+  //   //       setCurrentUser({
+  //   //         id: snapShot.id,
+  //   //         ...snapShot.data(),
+  //   //       });
+  //   //     });
+  //   //   } else {
+  //   //     //if we have no userAuth reset currentuser to null,
+  //   //     // this is also triggered by the auth.signOut() in the header
 
-    //     // setCurrentUser(null);
-    //     setCurrentUser(userAuth);
-    //   }
-    // });
-    //use this to update the shop collections (add new items to the shop)
-    // addCollectionAndDocuments(
-    //   "collections",
-    //   collections.map(({ title, items }) => ({ title, items }))
-    // );
-  }
+  //   //     // setCurrentUser(null);
+  //   //     setCurrentUser(userAuth);
+  //   //   }
+  //   // });
+  //   //use this to update the shop collections (add new items to the shop)
+  //   // addCollectionAndDocuments(
+  //   //   "collections",
+  //   //   collections.map(({ title, items }) => ({ title, items }))
+  //   // );
+  // }
 
-  componentWillUnmount() {
-    // only triggered to stop listening when leaving application.
-    this.unsubscribeFromAuth();
-  }
+  // componentWillUnmount() {
+  //   // only triggered to stop listening when leaving application.
+  //   this.unsubscribeFromAuth();
+  // }
 
-  render() {
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route exact path="/checkout" component={CheckOutContainer} />
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route exact path="/checkout" component={CheckOutContainer} />
 
-          <Route
-            exact
-            path="/signin"
-            render={() =>
-              this.props.currentUser ? (
-                <Redirect to="/" />
-              ) : (
-                <SignInAndSignUpPage />
-              )
-            }
-          />
-        </Switch>
-      </div>
-    );
-  }
-}
+        <Route
+          exact
+          path="/signin"
+          render={() =>
+            currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
+          }
+        />
+      </Switch>
+    </div>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
