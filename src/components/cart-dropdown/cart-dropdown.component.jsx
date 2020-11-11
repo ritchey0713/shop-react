a2468156257fe3cd85375fcb53412db52995a42c
Dj <ritchey0713@gmail.com>
2020-11-11 15:30:15 -0500

    refactor out withRouter HOC

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,33 +1,72 @@
 import React from "react";
 import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
+// import { withRouter } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import CartItem from "../cart-item/cart-item.component";
 import { selectCartitems } from "../../redux/cart/cart.selectors.js";
 import CustomButton from "../custom-button/custom-button.component";
 import "./cart-dropdown.styles.scss";
 import { createStructuredSelector } from "reselect";
 import { toggleCartHidden } from "../../redux/cart/cart.actions.js";
-import { StyledCartDropDown, StyledCartItems, StyledMessage } from "./cart-dropdown.styles";
+import {
+  StyledCartDropDown,
+  StyledCartItems,
+  StyledMessage,
+} from "./cart-dropdown.styles";
 
-const Cart = ({ cartItems, history, toggleCartHidden }) => (
-  <StyledCartDropDown>
-    <StyledCartItems>
-      {cartItems.length ? (
-        cartItems.map((item) => <CartItem key={item.id} item={item} />)
-      ) : (
-        <StyledMessage>The cart is empty</StyledMessage>
-      )}
-    </StyledCartItems>
-    <CustomButton
-      onClick={() => {
-        toggleCartHidden();
-        history.push("/checkout");
-      }}
-    >
-      CHECKOUT
-    </CustomButton>
-  </StyledCartDropDown>
-);
+// const Cart = ({ cartItems, history, toggleCartHidden }) => (
+//   <StyledCartDropDown>
+//     <StyledCartItems>
+//       {cartItems.length ? (
+//         cartItems.map((item) => <CartItem key={item.id} item={item} />)
+//       ) : (
+//         <StyledMessage>The cart is empty</StyledMessage>
+//       )}
+//     </StyledCartItems>
+//     <CustomButton
+//       onClick={() => {
+//         toggleCartHidden();
+//         history.push("/checkout");
+//       }}
+//     >
+//       CHECKOUT
+//     </CustomButton>
+//   </StyledCartDropDown>
+// );
+
+// // const mapStateToProps = ({ cart: { cartItems } }) => ({
+// //   cartItems,
+// // });
+
+// const mapStateToProps = createStructuredSelector({
+//   cartItems: selectCartitems,
+// });
+
+// //withRouter can take a comp as an arg, including connected comps
+// export default withRouter(connect(mapStateToProps, { toggleCartHidden })(Cart));
+
+const Cart = ({ cartItems, toggleCartHidden }) => {
+  let history = useHistory();
+  return (
+    <StyledCartDropDown>
+      <StyledCartItems>
+        {cartItems.length ? (
+          cartItems.map((item) => <CartItem key={item.id} item={item} />)
+        ) : (
+          <StyledMessage>The cart is empty</StyledMessage>
+        )}
+      </StyledCartItems>
+      <CustomButton
+        onClick={() => {
+          toggleCartHidden();
+          history.push("/checkout");
+        }}
+      >
+        CHECKOUT
+      </CustomButton>
+    </StyledCartDropDown>
+  );
+};
 
 // const mapStateToProps = ({ cart: { cartItems } }) => ({
 //   cartItems,
@@ -38,4 +77,4 @@ const mapStateToProps = createStructuredSelector({
 });
 
 //withRouter can take a comp as an arg, including connected comps
-export default withRouter(connect(mapStateToProps, { toggleCartHidden })(Cart));
+export default connect(mapStateToProps, { toggleCartHidden })(Cart);
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,43 @@
-import React from "react"
-import { withRouter } from 'react-router-dom'
-import { StyledContent, StyledImg, StyledMenuItem, StyledSubtitle, StyledTitle } from "./menu-item.styles"
-import "./menu-item.styles.scss"
+import React from "react";
+// import { withRouter } from 'react-router-dom'
+import { useHistory, useLocation } from "react-router-dom";
+import {
+  StyledContent,
+  StyledImg,
+  StyledMenuItem,
+  StyledSubtitle,
+  StyledTitle,
+} from "./menu-item.styles";
+import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => (
-  <StyledMenuItem className={size} onClick={() => history.push(`${match.url}${linkUrl}`)}>
-    <StyledImg imageUrl={imageUrl} className="background-image"/>
-    <StyledContent>
-      <StyledTitle>{ title.toUpperCase() }</StyledTitle>
-      <StyledSubtitle> SHOP NOW </StyledSubtitle>
-    </StyledContent>
-  </StyledMenuItem>
-)
+// const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => (
+//   <StyledMenuItem className={size} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+//     <StyledImg imageUrl={imageUrl} className="background-image"/>
+//     <StyledContent>
+//       <StyledTitle>{ title.toUpperCase() }</StyledTitle>
+//       <StyledSubtitle> SHOP NOW </StyledSubtitle>
+//     </StyledContent>
+//   </StyledMenuItem>
+// )
 
+// //gives access to history
+// export default withRouter(MenuItem)
+
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  let history = useHistory();
+  let url = useLocation();
+  return (
+    <StyledMenuItem
+      className={size}
+      onClick={() => history.push(`${url.pathname}${linkUrl}`)}
+    >
+      <StyledImg imageUrl={imageUrl} className="background-image" />
+      <StyledContent>
+        <StyledTitle>{title.toUpperCase()}</StyledTitle>
+        <StyledSubtitle> SHOP NOW </StyledSubtitle>
+      </StyledContent>
+    </StyledMenuItem>
+  );
+};
 //gives access to history
-export default withRouter(MenuItem)
+export default MenuItem;
