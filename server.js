5e4580ff88f82bc4377af231b17334cd33960893
Dj <ritchey0713@gmail.com>
2020-11-13 12:54:59 -0500

    add stripe post route

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require("path");
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -24,4 +26,19 @@ if (process.env.NODE_ENV === "production") {
 app.listen(port, (error) => {
   if (error) throw error;
   console.log("server running on " + port);
+});
+
+app.post("/payment", (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: "usd",
+  };
+  stripe.charges.create(body, (stripeError, stripeResp) => {
+    if (stripeError) {
+      res.status(500).send({ error: stripeError });
+    } else {
+      res.status(200).send({ success: stripeResp });
+    }
+  });
 });
