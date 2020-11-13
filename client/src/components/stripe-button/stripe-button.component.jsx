1992e9aa091a01b5c495a389e027462d1d73de55
Dj <ritchey0713@gmail.com>
2020-11-13 13:21:18 -0500

    server ready for deployment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
-
+import axios from "axios";
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_b2T1okYCs7N8suRfyieZ12aX00UFOXSVlF";
 
   const onToken = (token) => {
-    console.log(token);
+    // console.log(token);
     // token would be sent to backend for real payments
-    alert("Payment successful!");
+    // alert("Payment successful!");
+    axios({
+      url: "payment",
+      method: "POST",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((resp) => {
+        alert("payment success!", resp.data.success);
+      })
+      .catch((error) => {
+        console.log("payment error", error);
+        alert("issue with payment :(");
+      });
   };
 
   return (
