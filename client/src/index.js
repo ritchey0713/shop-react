b5ed11ac664c2c4f42083f0afe97c32bcb216550
Dj <ritchey0713@gmail.com>
2020-11-17 12:53:29 -0500

    test setup to back end

--- client/src/index.js
+++ client/src/index.js
@@ -4,18 +4,56 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/store";
+import { ApolloProvider } from "react-apollo";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient, gql } from "apollo-boost";
 import "./index.css";
 import App from "./App";
 
+//connection to backend
+
+const httpLink = createHttpLink({
+  uri: "https://crwn-clothing.com",
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+client
+  .query({
+    query: gql`
+      {
+        getCollectionsByTitle(title: "hats") {
+          id
+          title
+          items {
+            id
+            name
+            price
+            imageUrl
+          }
+        }
+      }
+    `,
+  })
+  .then((resp) => console.log(resp));
+
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <BrowserRouter>
-        <PersistGate persistor={persistor}>
-          <App />
-        </PersistGate>
-      </BrowserRouter>
-    </React.StrictMode>
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <React.StrictMode>
+        <BrowserRouter>
+          <PersistGate persistor={persistor}>
+            <App />
+          </PersistGate>
+        </BrowserRouter>
+      </React.StrictMode>
+    </Provider>
+  </ApolloProvider>,
   document.getElementById("root")
 );
