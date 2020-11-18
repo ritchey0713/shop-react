7265e6c33c76c606576c412f655d4c7453064ded
Dj <ritchey0713@gmail.com>
2020-11-18 11:17:50 -0500

    add gql container

---
+++ client/src/components/collections-overview/collections-overview.graphql-container.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
+import CollectionsOverview from "./collections-overview.component";
+import Spinner from "../spinner/spinner.component";
+import CollectionOverview from "./collections-overview.component";
+
+const GET_COLLECTIONS = gql`
+  {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        imageUrl
+        price
+      }
+    }
+  }
+`;
+
+const CollectionsOverviewContainer = () => (
+  <Query query={GET_COLLECTIONS}>
+    {({ loading, error, data }) => {
+      console.log({ loading });
+      console.log({ error });
+      console.log({ data });
+      if (loading) return <Spinner />;
+      return <CollectionOverview collections={data.collections} />;
+    }}
+  </Query>
+);
+
+export default CollectionsOverviewContainer;
