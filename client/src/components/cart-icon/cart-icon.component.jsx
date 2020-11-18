5e8e212f64f3c581a78a018076e90c9e14a17fd7
Dj <ritchey0713@gmail.com>
2020-11-18 12:29:57 -0500

    leverage graphql data

--- client/src/components/cart-icon/cart-icon.component.jsx
+++ client/src/components/cart-icon/cart-icon.component.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { connect } from "react-redux";
-
-import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { selectCartItemsCount } from "../../redux/cart/cart.selectors.js";
 import { createStructuredSelector } from "reselect";
@@ -26,4 +24,4 @@ const mapStateToProps = createStructuredSelector({
   itemCount: selectCartItemsCount,
 });
 
-export default connect(mapStateToProps, { toggleCartHidden })(CartIcon);
+export default connect(mapStateToProps)(CartIcon);
