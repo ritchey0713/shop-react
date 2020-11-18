571cb8b9a98e36240cd86d090434d49425f28e6a
Dj <ritchey0713@gmail.com>
2020-11-18 12:30:06 -0500

    load header container

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import { connect } from "react-redux";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import Header from "./components/header/header.component";
+import { default as Header } from "./components/header/header.container";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 //import CheckoutPage from "./pages/check-out/check-out.component";
 import { createStructuredSelector } from "reselect";
