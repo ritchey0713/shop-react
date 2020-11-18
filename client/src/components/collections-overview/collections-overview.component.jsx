1862831e734b6342522e24475d49e2b82d875fe4
Dj <ritchey0713@gmail.com>
2020-11-18 11:18:01 -0500

    rebuild overview comp with gql

--- client/src/components/collections-overview/collections-overview.component.jsx
+++ client/src/components/collections-overview/collections-overview.component.jsx
@@ -1,3 +1,33 @@
+//pre graphql
+
+// import React from "react";
+// import { connect } from "react-redux";
+// import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
+// import { createStructuredSelector } from "reselect";
+// import CollectionPreview from "../collection-preview/collection-preview.component";
+// import "./collections-overview.styles.scss";
+// import { StyledOverview } from "./collections-overview.styles";
+
+// const CollectionOverview = ({ collections, ...otherProps }) => {
+//   return (
+//     <StyledOverview>
+//       {collections.map(({ id, ...otherCollectionProps }) => (
+//         <CollectionPreview
+//           key={id}
+//           path={otherProps.match.path}
+//           {...otherCollectionProps}
+//         />
+//       ))}
+//     </StyledOverview>
+//   );
+// };
+
+// const mapStateToProps = createStructuredSelector({
+//   collections: selectCollectionsForPreview,
+// });
+
+// export default connect(mapStateToProps)(CollectionOverview);
+
 import React from "react";
 import { connect } from "react-redux";
 import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
@@ -5,23 +35,16 @@ import { createStructuredSelector } from "reselect";
 import CollectionPreview from "../collection-preview/collection-preview.component";
 import "./collections-overview.styles.scss";
 import { StyledOverview } from "./collections-overview.styles";
+import { useHistory } from "react-router-dom";
 
 const CollectionOverview = ({ collections, ...otherProps }) => {
   return (
     <StyledOverview>
       {collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview
-          key={id}
-          path={otherProps.match.path}
-          {...otherCollectionProps}
-        />
+        <CollectionPreview key={id} {...otherProps} {...otherCollectionProps} />
       ))}
     </StyledOverview>
   );
 };
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollectionsForPreview,
-});
-
-export default connect(mapStateToProps)(CollectionOverview);
+export default CollectionOverview;
