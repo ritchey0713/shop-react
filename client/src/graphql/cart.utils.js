fa0d4357a01143a0edc9d680ba96c5c593d9e095
Dj <ritchey0713@gmail.com>
2020-11-18 14:44:54 -0500

    add cart utils

---
+++ client/src/graphql/cart.utils.js
@@ -0,0 +1,30 @@
+export const addItemToCart = (cartItems, cartitemToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartitemToAdd.id
+  );
+  console.log(cartItems);
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartitemToAdd.id
+        ? { ...cartItem, quantity: (cartItem.quantity += 1) }
+        : { ...cartItem }
+    );
+  }
+  //only hits this if the no match is found for item being added
+  return [...cartItems, { ...cartitemToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
