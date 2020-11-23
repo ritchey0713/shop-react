37531e7b0630cda3d9c98ed8d2d2ddbd340a7328
Dj <ritchey0713@gmail.com>
2020-11-23 14:28:29 -0500

    change how with spionner loads

--- client/src/components/with-spinner/with-spinner.component.jsx
+++ client/src/components/with-spinner/with-spinner.component.jsx
@@ -1,15 +1,10 @@
 import React from "react";
+import Spinner from "../spinner/spinner.component";
 import { SpinnerContainer, SpinnerOverlay } from "./with-spinner.styles";
 // the first thing returned ie: ({ isLoading, ...otherProps }) is from whatever component calls on withSpinner
 
 const WithSpinner = (WrappedComponent) => ({ isLoading, ...otherProps }) => {
-  return isLoading ? (
-    <SpinnerOverlay>
-      <SpinnerContainer />
-    </SpinnerOverlay>
-  ) : (
-    <WrappedComponent {...otherProps} />
-  );
+  return isLoading ? <Spinner /> : <WrappedComponent {...otherProps} />;
 };
 
 export default WithSpinner;
