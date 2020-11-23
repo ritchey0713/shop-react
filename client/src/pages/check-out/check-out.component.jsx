b465364f3141317f61098b64adfb77472114de61
Dj <ritchey0713@gmail.com>
2020-11-23 10:11:26 -0500

    add logs to test

--- client/src/pages/check-out/check-out.component.jsx
+++ client/src/pages/check-out/check-out.component.jsx
@@ -9,38 +9,41 @@ import {
   StyledTotal,
 } from "./check-out.styles.jsx";
 
-const CheckoutPage = ({ cartItems, cartTotal }) => (
-  <StyledCheckout>
-    <StyledHeader>
-      <StyledHeaderBlock>
-        <span>Product</span>
-      </StyledHeaderBlock>
-      <StyledHeaderBlock>
-        <span className="">Description</span>
-      </StyledHeaderBlock>
-      <StyledHeaderBlock>
-        <span className="">Quantity</span>
-      </StyledHeaderBlock>
-      <StyledHeaderBlock>
-        <span className="">Price</span>
-      </StyledHeaderBlock>
-      <StyledHeaderBlock>
-        <span className="">REMOVE</span>
-      </StyledHeaderBlock>
-    </StyledHeader>
-    {cartItems.map((item) => (
-      <CheckoutItem key={item.id} cartItem={item} />
-    ))}
-    <StyledTotal>
-      <span>TOTAL: ${cartTotal}</span>
-    </StyledTotal>
-    <StyledTestWarning>
-      *Please use the following test credit card for payments*
-      <br />
-      4242 4242 4242 4242 - exp: 01/21 - cvv: 123
-    </StyledTestWarning>
-    <StripeCheckoutButton price={cartTotal} />
-  </StyledCheckout>
-);
+const CheckoutPage = ({ cartItems, cartTotal }) => {
+  console.log(cartItems);
+  return (
+    <StyledCheckout>
+      <StyledHeader>
+        <StyledHeaderBlock>
+          <span>Product</span>
+        </StyledHeaderBlock>
+        <StyledHeaderBlock>
+          <span className="">Description</span>
+        </StyledHeaderBlock>
+        <StyledHeaderBlock>
+          <span className="">Quantity</span>
+        </StyledHeaderBlock>
+        <StyledHeaderBlock>
+          <span className="">Price</span>
+        </StyledHeaderBlock>
+        <StyledHeaderBlock>
+          <span className="">REMOVE</span>
+        </StyledHeaderBlock>
+      </StyledHeader>
+      {cartItems.map((item) => (
+        <CheckoutItem key={item.id} cartItem={item} />
+      ))}
+      <StyledTotal>
+        <span>TOTAL: ${cartTotal}</span>
+      </StyledTotal>
+      <StyledTestWarning>
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - exp: 01/21 - cvv: 123
+      </StyledTestWarning>
+      <StripeCheckoutButton price={cartTotal} />
+    </StyledCheckout>
+  );
+};
 
 export default CheckoutPage;
